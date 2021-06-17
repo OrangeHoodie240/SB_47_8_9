e13e6fbf77ec7c446c90a08bf13982ac7fb4386c
steven daddario <daddarios@dupage.edu>
2021-06-16 20:19:51 -0500

    finsihed binary-tree.js up to the extra further study section. I commented out the tests for the further study methods

--- binary-tree.js
+++ binary-tree.js
@@ -17,28 +17,159 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth() {
+    if(!this.root){
+      return 0;
+    }
 
+    const queue = [];
+    queue.push([this.root, 1]); 
+
+    while(queue.length > 0){
+      let node = queue.shift();
+      if(!node[0].left && !node[0].right){
+        return node[1]; 
+      }
+
+      if(node[0].left){
+        queue.push([node[0].left, node[1] + 1]);
+      }
+      if(node[0].right){
+        queue.push([node[0].right, node[1] + 1]);
+      }
+
+    }
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
   maxDepth() {
+    if(!this.root){
+      return 0;
+    }
 
+    const queue = [];
+    queue.push([this.root, 1]); 
+    let max = 1; 
+
+    while(queue.length > 0){
+      let node = queue.shift();
+
+      if(node[0].left || node[0].right){
+        if(node[1] + 1 > max){
+          max = node[1] + 1; 
+        }
+      }
+
+      if(node[0].left){
+        queue.push([node[0].left, node[1] + 1]);
+      }
+      if(node[0].right){
+        queue.push([node[0].right, node[1] + 1]);
+      }
+
+    }
+    return max; 
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
+    if(!this.root){
+      return 0; 
+    }
+    let max = null;
+    let stack = []; 
+    stack.push(this.root); 
+    while(stack.length > 0){
+      let node = stack.pop(); 
+      let val = _maxSum(node); 
+      if(max === null || val > max){
+        max = val;
+      }
+      
+      if(node.left){
+        stack.push(node.left);
+      }
+      if(node.right){
+        stack.push(node.right);
+      }
 
+    }
+    return max;       
+
+    function _maxSum(node){
+      let stack = []; 
+      let max = node.val;
+
+      let left = node.left; 
+      let right = node.right; 
+      if(left){
+        let val = node.val + left.val + ((right && right.val > 0) ? right.val : 0);
+        stack.push([left, val]);
+        if(val > max){
+          max = val;
+        }        
+      }
+      if(right){
+        let val = node.val + right.val + ((left && left.val > 0) ? left.val : 0);
+        stack.push([right, val]);
+        if(val > max){
+          max = val;
+        }
+      }
+      
+      while(stack.length > 0){
+        node = stack.pop(); 
+        if(!node[0].left && !node[0].right){
+          if(node[1] > max){
+            max = node[1];
+          }
+        }
+
+        if(node[0].right){
+          stack.push([node[0].right, node[1] + node[0].right.val]); 
+        }
+
+        if(node[0].left){
+          stack.push([node[0].left, node[1] + node[0].left.val]); 
+
+        }
+      }
+      
+      return max; 
+    }
+    
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound) {
+    if(!this.root){
+      return null; 
+    }
 
+    let stack = [];
+    let current = null;  
+    stack.push(this.root); 
+    while(stack.length > 0){
+      let node = stack.pop(); 
+      if(node.val > lowerBound){
+        if(!current || node.val < current){
+          current  = node.val;
+        }
+      }
+      if(node.left){
+        stack.push(node.left); 
+      }
+
+      if(node.right){
+        stack.push(node.right);
+      }
+    }
+    return current;
   }
 
   /** Further study!
--- binary-tree.test.js
+++ binary-tree.test.js
@@ -99,124 +99,128 @@ describe("nextLarger", function() {
   });
 });
 
-describe("areCousins", function() {
-  it("returns true if they are cousins, false if not", function() {
-    let n7 = new BinaryTreeNode(7);
-    let n6 = new BinaryTreeNode(6);
-    let n5 = new BinaryTreeNode(5);
-    let n4 = new BinaryTreeNode(4);
-    let n3 = new BinaryTreeNode(3, n6, n7);
-    let n2 = new BinaryTreeNode(2, n4, n5);
-    let root = new BinaryTreeNode(1, n2, n3);
-    let tree = new BinaryTree(root);
 
-    expect(tree.areCousins(n4, n6)).toBe(true);
-    expect(tree.areCousins(n4, n7)).toBe(true);
-    expect(tree.areCousins(n5, n6)).toBe(true);
-    expect(tree.areCousins(n5, n7)).toBe(true);
-    expect(tree.areCousins(n2, n3)).toBe(false);
-    expect(tree.areCousins(n4, n5)).toBe(false);
-    expect(tree.areCousins(n6, n7)).toBe(false);
-    expect(tree.areCousins(n4, n3)).toBe(false);
-    expect(tree.areCousins(root, n3)).toBe(false);
-  });
-});
+// *** COMMENTING OUT THE FURTHER STUDY TESTS *** 
 
-describe("serialize and deserialize", function() {
-  let myTree;
 
-  beforeEach(function() {
-    let root = new BinaryTreeNode(1);
-    root.left = new BinaryTreeNode(2);
-    root.right = new BinaryTreeNode(3);
-    root.right.left = new BinaryTreeNode(4);
-    root.right.right = new BinaryTreeNode(5);
+// describe("areCousins", function() {
+//   it("returns true if they are cousins, false if not", function() {
+//     let n7 = new BinaryTreeNode(7);
+//     let n6 = new BinaryTreeNode(6);
+//     let n5 = new BinaryTreeNode(5);
+//     let n4 = new BinaryTreeNode(4);
+//     let n3 = new BinaryTreeNode(3, n6, n7);
+//     let n2 = new BinaryTreeNode(2, n4, n5);
+//     let root = new BinaryTreeNode(1, n2, n3);
+//     let tree = new BinaryTree(root);
 
-    myTree = new BinaryTree(root);
-  });
+//     expect(tree.areCousins(n4, n6)).toBe(true);
+//     expect(tree.areCousins(n4, n7)).toBe(true);
+//     expect(tree.areCousins(n5, n6)).toBe(true);
+//     expect(tree.areCousins(n5, n7)).toBe(true);
+//     expect(tree.areCousins(n2, n3)).toBe(false);
+//     expect(tree.areCousins(n4, n5)).toBe(false);
+//     expect(tree.areCousins(n6, n7)).toBe(false);
+//     expect(tree.areCousins(n4, n3)).toBe(false);
+//     expect(tree.areCousins(root, n3)).toBe(false);
+//   });
+// });
 
-  it("serializes trees into strings", function() {
-    // Failure message:
-    // The 'serialize' function needs to output a string.
+// describe("serialize and deserialize", function() {
+//   let myTree;
 
-    expect(typeof BinaryTree.serialize(myTree)).toBe("string");
-  });
+//   beforeEach(function() {
+//     let root = new BinaryTreeNode(1);
+//     root.left = new BinaryTreeNode(2);
+//     root.right = new BinaryTreeNode(3);
+//     root.right.left = new BinaryTreeNode(4);
+//     root.right.right = new BinaryTreeNode(5);
 
-  it("deserializes strings into BinaryTree objects", function() {
-    // Failure message:
-    // The 'deserialize' function needs to output a BinaryTreeNode
+//     myTree = new BinaryTree(root);
+//   });
 
-    let serialized = BinaryTree.serialize(myTree);
-    let result = BinaryTree.deserialize(serialized);
-    expect(result instanceof BinaryTree).toBe(true);
-  });
+//   it("serializes trees into strings", function() {
+//     // Failure message:
+//     // The 'serialize' function needs to output a string.
 
-  it("reverses one another", function() {
-    // Failure message:
-    // the function 'deserialize' should perfectly reverse the function 'serialize'
+//     expect(typeof BinaryTree.serialize(myTree)).toBe("string");
+//   });
 
-    let serialized = BinaryTree.serialize(myTree);
-    let result = BinaryTree.deserialize(serialized);
-    expect(result).toEqual(myTree);
-  });
+//   it("deserializes strings into BinaryTree objects", function() {
+//     // Failure message:
+//     // The 'deserialize' function needs to output a BinaryTreeNode
 
-  it("is a pure function", function() {
-    // Failure message:
-    // original tree must be unchanged
+//     let serialized = BinaryTree.serialize(myTree);
+//     let result = BinaryTree.deserialize(serialized);
+//     expect(result instanceof BinaryTree).toBe(true);
+//   });
 
-    let root = new BinaryTreeNode(1);
-    root.left = new BinaryTreeNode(2);
-    root.right = new BinaryTreeNode(3);
-    root.right.left = new BinaryTreeNode(4);
-    root.right.right = new BinaryTreeNode(5);
+//   it("reverses one another", function() {
+//     // Failure message:
+//     // the function 'deserialize' should perfectly reverse the function 'serialize'
 
-    myTreeCopy = new BinaryTree(root);
+//     let serialized = BinaryTree.serialize(myTree);
+//     let result = BinaryTree.deserialize(serialized);
+//     expect(result).toEqual(myTree);
+//   });
 
-    let serialized = BinaryTree.serialize(myTree);
-    BinaryTree.deserialize(serialized);
+//   it("is a pure function", function() {
+//     // Failure message:
+//     // original tree must be unchanged
 
-    expect(myTree).toEqual(myTreeCopy);
-  });
-});
+//     let root = new BinaryTreeNode(1);
+//     root.left = new BinaryTreeNode(2);
+//     root.right = new BinaryTreeNode(3);
+//     root.right.left = new BinaryTreeNode(4);
+//     root.right.right = new BinaryTreeNode(5);
 
-describe("lowestCommonAncestor", function() {
-  it("returns the lowest common ancestor", function() {
-    // Failure message:
-    // failed for tree (same as test examples)
-    const root = new BinaryTreeNode(3);
-    const tree = new BinaryTree(root);
+//     myTreeCopy = new BinaryTree(root);
 
-    /* build left subtree */
+//     let serialized = BinaryTree.serialize(myTree);
+//     BinaryTree.deserialize(serialized);
 
-    const left = new BinaryTreeNode(5);
-    root.left = left;
+//     expect(myTree).toEqual(myTreeCopy);
+//   });
+// });
 
-    const leftLeft = new BinaryTreeNode(6);
-    left.left = leftLeft;
+// describe("lowestCommonAncestor", function() {
+//   it("returns the lowest common ancestor", function() {
+//     // Failure message:
+//     // failed for tree (same as test examples)
+//     const root = new BinaryTreeNode(3);
+//     const tree = new BinaryTree(root);
 
-    const leftRight = new BinaryTreeNode(2);
-    left.right = leftRight;
+//     /* build left subtree */
 
-    const leftRightLeft = new BinaryTreeNode(7);
-    leftRight.left = leftRightLeft;
+//     const left = new BinaryTreeNode(5);
+//     root.left = left;
 
-    const leftRightRight = new BinaryTreeNode(4);
-    leftRight.right = leftRightRight;
+//     const leftLeft = new BinaryTreeNode(6);
+//     left.left = leftLeft;
 
-    /* build right subtree */
+//     const leftRight = new BinaryTreeNode(2);
+//     left.right = leftRight;
 
-    const right = new BinaryTreeNode(1);
-    root.right = right;
+//     const leftRightLeft = new BinaryTreeNode(7);
+//     leftRight.left = leftRightLeft;
 
-    const right_left = new BinaryTreeNode(0);
-    right.left = right_left;
+//     const leftRightRight = new BinaryTreeNode(4);
+//     leftRight.right = leftRightRight;
 
-    const right_right = new BinaryTreeNode(8);
-    right.right = right_right;
+//     /* build right subtree */
 
-    expect(tree.lowestCommonAncestor(left, right)).toBe(root);
-    expect(tree.lowestCommonAncestor(leftRight, leftRightLeft)).toBe(leftRight);
-    expect(tree.lowestCommonAncestor(leftRightLeft, leftLeft)).toBe(left);
-    expect(tree.lowestCommonAncestor(right_left, right_right)).toBe(right);
-  });
-});
+//     const right = new BinaryTreeNode(1);
+//     root.right = right;
+
+//     const right_left = new BinaryTreeNode(0);
+//     right.left = right_left;
+
+//     const right_right = new BinaryTreeNode(8);
+//     right.right = right_right;
+
+//     expect(tree.lowestCommonAncestor(left, right)).toBe(root);
+//     expect(tree.lowestCommonAncestor(leftRight, leftRightLeft)).toBe(leftRight);
+//     expect(tree.lowestCommonAncestor(leftRightLeft, leftLeft)).toBe(left);
+//     expect(tree.lowestCommonAncestor(right_left, right_right)).toBe(right);
+//   });
+// });
